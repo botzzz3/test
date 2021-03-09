22de9c31e762d44ca61681173de923a4aaf2291a
botzzz3 <77111954+botzzz3@users.noreply.github.com>
2021-03-08 23:08:40 -0300

    Add files via upload

---
+++ src/charope.js
@@ -0,0 +1,32 @@
+const charope = (prefix, sender) => {
+	return `c͟o͟m͟a͟n͟d͟o͟ d͟o͟ b͟o͟t͟ :)
+
+ l͟e͟b͟r͟a͟n͟d͟o͟ o͟s͟ c͟o͟m͟a͟n͟d͟o͟s͟ e͟s͟t͟ã͟o͟ e͟m͟ t͟e͟s͟t͟e͟ ✌︎
+𝘦𝘴𝘱𝘦𝘳𝘰 𝘲𝘶𝘦 𝘨𝘰𝘴𝘵𝘦
+𝘤𝘳𝘪𝘢𝘥𝘰𝘳 𝘥𝘰 𝘣𝘰𝘵:𝘣𝘺  𝘤𝘩𝘢𝘳𝘰𝘱𝘦
+WA.me/447520621694
+------------------
+use (!) para cada comando 
+comandos que funfa☑️
+condos que nao funfa❎
+c͟o͟m͟a͟n͟d͟o͟s͟ e͟m͟ d͟e͟s͟e͟n͟v͟o͟l͟v͟i͟m͟e͟n͟t͟o͟s͟🔰
+☑️sticker(converter imagme,gif em figurinhas
+☑️tts(use /tts para ver as línguas)
+☑️toimg(converter figu em imgens 
+☑️ 
+    espere para + comandos
+------------------------------------
+❎codornar
+❎ceifador
+❎mamao
+❎pedro
+❎terra
+-----------------------------------
+🔰codornar(vai c criado)
+🔰ceifador(vai c criado)
+🔰mamao(vai c criado)
+🔰pedro(vai c criado)
+🔰terra(vai c criado)'
+}
+
+exports.charope = charope
